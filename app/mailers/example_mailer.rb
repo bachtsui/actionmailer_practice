abb837ccf1a0c86a2dd61026939d619fb3e12966
bachtsui <dbtsui2@gmail.com>
2016-01-27 10:56:51 -0800

    Presentable mode

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,8 +1,9 @@
 class ExampleMailer < ActionMailer::Base
+	# default from: "dbtsui@gmail.com"
 	default from: "joeroerskawaii@gmail.com"
 
 	def sample_email(user)
 		@user = user
-		mail(to: @user.email, subject: "Test Email")
+		mail(to: @user.email, subject: "Gem Test!")
 	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => ENV['joeroerskawaii@gmail.com'],
+    :password             => ENV['throwaway'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
